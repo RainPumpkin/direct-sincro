97a64ed94addc572bd5d2ba9ad9eaa5bcdda1d33
RainPumpkin <manuel2mgsilva@gmail.com>
2022-05-29 17:23:39 +0100

    fixed last commit

---
+++ server/direct-sincro/src/main/kotlin/ps/g08/directsincro/controller/AlugadosController.kt
@@ -0,0 +1,11 @@
+package ps.g08.directsincro.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/*
+May be later merged with veiculos
+ */
+@RestController
+@RequestMapping("/api/subscritores/{nif}/alugados")
+class AlugadosController {}
---
+++ server/direct-sincro/src/main/kotlin/ps/g08/directsincro/controller/PedidoDefesaController.kt
@@ -0,0 +1,12 @@
+package ps.g08.directsincro.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PedidoDefesaController {
+    //subscritores/nif/veiculos/matricula/eventos/numeroauto/pedido ->pedido de defesa desse evento. usado para GET e Post
+
+    //subscritores/nif/pedido_defesas/
+}
