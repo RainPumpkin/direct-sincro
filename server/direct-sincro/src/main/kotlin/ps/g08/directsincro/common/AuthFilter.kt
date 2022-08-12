204264381c82a07487c5ccc730f6c596136277bf
RainPumpkin <manuel2mgsilva@gmail.com>
2022-08-12 19:37:39 +0100

    finished login/auth implementation

---
+++ server/direct-sincro/src/main/kotlin/ps/g08/directsincro/common/AuthFilter.kt
@@ -0,0 +1,44 @@
+package ps.g08.directsincro.common
+
+import ps.g08.directsincro.service.SubscritorService
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class AuthFilter(private val cookieManager: CookieManager, private val subscritorService: SubscritorService): Filter {
+
+    private fun respondWithUnauthorized(httpResponse: HttpServletResponse, reason: String? = null) {
+        httpResponse.status = HttpServletResponse.SC_UNAUTHORIZED
+        httpResponse.addHeader("Content-Type", "application/json")
+        httpResponse.writer?.write(ErrorMessage(
+            code = 403,
+            title = "Access denied",
+            description = reason
+        ).toString())
+    }
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        //ESTÁ ASSIM PARA N PRECISARMOS DO COOKIE PARA TESTES
+        if(true) chain.doFilter(request, response)
+        else {
+            val httpRequest = request as HttpServletRequest
+            val httpResponse = response as HttpServletResponse
+
+            val cookies = httpRequest.cookies
+            if (cookies == null) return respondWithUnauthorized(httpResponse, "User don't have cookie")
+
+            val cookie = request.cookies.find { it.name == "Authorization" }
+            if (cookie == null) return respondWithUnauthorized(httpResponse, "User don't have cookie")
+
+            val user = cookieManager.getUser(cookie.value)
+            if (user == null) return respondWithUnauthorized(httpResponse, "User does not exist")
+
+            if (subscritorService.checkPassword(user.nif, user.password)) chain.doFilter(request, response)
+            else return respondWithUnauthorized(httpResponse, "User invalid credentials")
+        }
+
+    }
+}
--- server/direct-sincro/src/main/kotlin/ps/g08/directsincro/database/SubscritorDatabase.kt
+++ server/direct-sincro/src/main/kotlin/ps/g08/directsincro/database/SubscritorDatabase.kt
@@ -4,6 +4,8 @@ import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.withHandleUnchecked
 import org.springframework.stereotype.Component
 import ps.g08.directsincro.common.Subscritor
+import ps.g08.directsincro.common.getTimestamp
+import java.sql.Timestamp
 
 data class SubscritorDatabaseRow(
     val nif: String
@@ -14,7 +16,10 @@ class SubscritorDatabase(private val source : Jdbi) {
     companion object {
         const val queryGet = "SELECT * FROM Subscritor WHERE nif = ? "//?
         //const val queryGetAll = "SELECT * FROM Subscritor"
+        const val queryGetLastSubs = "Select inicio FROM DataSubscricao WHERE nif = ? AND cancelamento = null "
         const val queryCreate = "INSERT INTO Subscritor(nif) VALUES (?)"
+        const val queryCreateData = "INSERT INTO DataSubscricao(nif, inicio) VALUES (?, ?)"
+        const val queryUpdate = "UPDATE DataSubscricao SET cancelamento = ? WHERE nif = ? AND inicio = ?"
         const val queryDelete = "Delete FROM Subscritor WHERE nif = ?"
     }
 
@@ -29,20 +34,48 @@ class SubscritorDatabase(private val source : Jdbi) {
     }
 
     fun create(nif: String): String {
-        source.withHandleUnchecked { handle ->
-            handle
-                .createUpdate(queryCreate)
-                .bind(0, nif)
-                .execute()
+        try {
+            source.withHandleUnchecked { handle ->
+                handle
+                    .createUpdate(queryCreate)
+                    .bind(0, nif)
+                    .execute()
+            }
+        } catch(_: Exception) {
+            //já existia subscritor, n faz mal
+        }
+        val curr = getTimestamp(System.currentTimeMillis())
+        source.withHandleUnchecked { handle -> handle
+            .createUpdate(queryCreateData)
+            .bind(0, nif)
+            .bind(1, curr)
+            .execute()
         }
         return nif
     }
 
     fun delete(nif : String) {
+        val time: Timestamp = source.withHandleUnchecked { handle -> handle
+            .createQuery(queryGetLastSubs)
+            .bind(0, nif)
+            .mapTo(Timestamp::class.java)
+            .one()
+        }
+
+        val curr = getTimestamp(System.currentTimeMillis())
+
+        source.withHandleUnchecked { handle -> handle
+            .createUpdate(queryUpdate)
+            .bind(0, curr)
+            .bind(1, nif)
+            .bind(2, time)
+            .execute()
+        }
+        /*explicit delete
         source.withHandleUnchecked { handle -> handle
             .createUpdate(queryDelete)
             .bind(0, nif)
             .execute()
-        }
+        }*/
     }
 }
